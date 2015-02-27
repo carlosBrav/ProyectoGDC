71e7f16b7f97e404729726ea4c4e0585d8e4e31e
carlosBrav <carlos.br.carb@gmail.com>
2015-02-26 19:25:16 -0500

    proyectoGDC1

---
+++ template-spring-primefaces-hibernate-maven-master/src/main/java/org/czegarram/template/service/IUserService.java
@@ -0,0 +1,18 @@
+package org.czegarram.template.service;
+
+import java.util.List;
+
+import org.czegarram.template.domain.User;
+
+public interface IUserService {
+
+	public void addUser(User user);
+
+	public void updateUser(User user);
+
+	public void deleteUser(User user);
+
+	public User getUserById(int id);
+
+	public List<User> getUsers();
+}
